84c1958af62f45351f9ebfbc59cceb48d0b8c339
systemuser <admin@enagednation.com>
2022-04-07 21:03:56 +0000

    Updating template for new reach client

--- RoboFile.php
+++ RoboFile.php
@@ -6,7 +6,7 @@
 /**
  * @author Joshua Copeland <josh@engagednation.com>
  * @author Matthew Kosolofski <matthew.kosolofski@engagednation.com>
- * @package rampart
+ * @package rampart-10k
  */
 class RoboFile extends \Robo\Tasks
 {
